fb7c4cda7c85ab5285fdc6f3e5616367ea7b9973
root <you@example.com>
2024-06-19 17:43:46 +0000

    a

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,12 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i <= 57; i++)
-    {
-        putchar(i);
-    }
-
-    putchar('\n');
-    
-    return (0);
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
