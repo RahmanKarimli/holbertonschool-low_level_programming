62d669f36ad9a419e3af95eaa67a73e327228c6e
root <you@example.com>
2024-06-20 18:07:27 +0000

    a

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		_putchar('\n ');
+		return 1;
+	}
+
+	else
+	{
+		_putchar('\n ');
+		return 0;
+	}
+}
