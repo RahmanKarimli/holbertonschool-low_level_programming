b8ba0fdc8226b1f73decc0ce466a3665a58fa50c
root <you@example.com>
2024-06-20 14:37:52 +0000

    a

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,9 +5,11 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void) {
+void print_alphabet(void)
+{
 	int i;
-	for (i = 97; i < 123; i++) {
+	for (i = 97; i < 123; i++)
+	{
 		_putchar(i);
 	}
 	_putchar('/n');
