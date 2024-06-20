087ed3160dc93e386eb4a5305427dc28fc24199f
root <you@example.com>
2024-06-20 14:35:12 +0000

    a

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void) {
+	int i;
+	for (i = 97; i < 123; i++) {
+		_putchar(i);
+	}
+	_putchar('/n');
+}
