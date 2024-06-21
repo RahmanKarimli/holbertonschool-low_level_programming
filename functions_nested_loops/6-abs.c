3b5bde9f8f3cb544c80f4945c02f81995e3a608b
root <you@example.com>
2024-06-21 16:32:05 +0000

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: integer
+ * Return:0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		_putchar(n);
+	}
+
+	else
+	{
+		n = -1 * n;
+		_putchar(n);
+	}
+}
