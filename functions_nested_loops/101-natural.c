495e05a2517cbc0db37e9334f462f282be99bf8e
root <you@example.com>
2024-06-22 15:24:39 +0000

    a

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of all multiples of 3 or 5 up to 1024
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int i, n;
+
+	for (i = 2; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			n += i;
+	}
+	printf("%d\n", n);
+}
