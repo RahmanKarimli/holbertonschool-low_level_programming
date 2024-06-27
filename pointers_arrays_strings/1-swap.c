47d1a8d67f9daa84aa2ee3d2f5e13788691bfcdf
root <you@example.com>
2024-06-27 19:15:29 +0000

    a

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * @a: integer
+ * @b: integer
+ * Return:0
+ */
+void swap_int(int *a, int *b)
+{
+        int c;
+	
+	c = *a;
+	*a = b;
+	*b = c;
+}
