2ba29e796559c653f476dced1fe4d5c205b44186
root <you@example.com>
2024-06-29 19:24:27 +0000

    a

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * string_toupper - Entry point
+ * @a: array
+ * Return:0
+ */
+char *string_toupper(char *a)
+{
+	int i, len;
+
+	for (len = 0; a[len] != '\0'; len++)
+	{
+	}
+	for (int i = 0; i < len; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			*(a + i) = a[i] - 32;
+		}
+		else
+		{
+			*(a + i) = *(a + i);
+		}
+	}
+	return (a);
+}
