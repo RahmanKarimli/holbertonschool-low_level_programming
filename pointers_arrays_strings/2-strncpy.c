5c78d10d1fb38b755a18adb75ce2b9fa1ae2e258
root <you@example.com>
2024-06-29 15:49:47 +0000

    a

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, lens;
 
+	for (lens = 0; src[lens] != '\0'; lens++)
+	{
+	}
 	for (i = 0; i < n; i++)
 	{
+		if (lens < 1)
+		{
+			break;
+		}
 	dest[i] = src[i];
 	}
 	return (dest);
