ee49c79a3aa9c3ba63559745ffcad56bf8e9a78b
root <you@example.com>
2024-06-29 14:55:39 +0000

    a

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - Entry point
+ * @src: string
+ * @dest: string
+ * @n: integer
+ * Return:0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lend, lens, i;
+
+	lend = 0;
+	lens = 0;
+	for (lend = 0; dest[lend] != '\0'; lend++)
+	{
+	}
+	for (lens = 0; src[lens] != '\0'; lens++)
+	{
+	}
+	for (i = 0; i <= n - 1; i++)
+	{
+		*(dest + lend) = *(src + i);
+		lend++;
+	}
+	dest[lend] = '\0';
+	return (dest);
+}
