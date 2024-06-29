1cc96486f8056b257469fa071bc7ebc44d559613
root <you@example.com>
2024-06-29 15:45:50 +0000

    a

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - Entry point
+ * @src: string
+ * @dest: string
+ * @n: integer
+ * Return:0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
