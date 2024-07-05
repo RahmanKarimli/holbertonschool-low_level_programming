4783e827fe90c6c8c330fddfae4377159f9a7fe8
root <you@example.com>
2024-07-05 15:47:25 +0000

    a

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return:0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s;
+	int s2;
+
+	s = 0;
+	s2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		s += *(a + i * size + i);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s2 += *(a + i * size + (size - i - 1));
+	}
+	printf("%d, %d", s, s2);
+	printf("\n");
+}
