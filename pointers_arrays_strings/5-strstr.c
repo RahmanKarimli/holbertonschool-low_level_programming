dfe75f731fe9b98cddc1fbea71ace3dcb250936e
root <you@example.com>
2024-07-05 14:12:36 +0000

    a

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * _strpbrk - Entry point
- * @s: string
- * @accept: string
+ * _strstr - Entry point
+ * @haystack: string
+ * @needle: string
  * Return:0
  */
-char *_strstr(char *haystack, char *nedle)
+char *_strstr(char *haystack, char *needle)
 {
-	
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[i + j] == needle[j]; j++)
+		{
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
 }
