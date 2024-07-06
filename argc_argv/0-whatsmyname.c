e086b7bfff86b4cdfe222f42fc25be2907430bbb
root <you@example.com>
2024-07-06 14:56:31 +0000

    a

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the alphabet in lowercase, followed by a new line
+ * @argc: integer
+ * @argv: array
  * Return: Always 0 (Success)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 
