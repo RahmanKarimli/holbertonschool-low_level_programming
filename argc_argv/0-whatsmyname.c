e2d4cfcd565c08293d99b10dad16f863092c91ef
root <you@example.com>
2024-07-06 14:47:41 +0000

    a

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+#include "main.h"
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
