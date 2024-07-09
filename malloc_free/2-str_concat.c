2e6e9ab47e3dc29033d8b2357b3c46a764cf17b2
root <you@example.com>
2024-07-09 17:51:17 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,11 +8,20 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len;
-	int len2;
 	char *concat;
-	int i, j;
-
+	int i, j, len, lens2;
+	if (s1)
+	{
+		for (; s1[len] != 0; len++)
+		{
+		}
+	}
+	if (s2)
+	{
+		for (; s2[len2] != 0; len2++)
+		{
+		}
+	}
 	concat = malloc(sizeof(char) * (len + len2 + 1));
 	if (concat == NULL)
 	{
@@ -20,9 +29,6 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s1)
 	{
-		for (len = 0; s1[len] != 0; len++)
-		{
-		}
 		for (i = 0; i < len; i++)
 		{
 			concat[i] = s1[i];
@@ -30,13 +36,10 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2)
 	{
-		for (len2 = 0; s2[len2] != 0; len2++)
-		{
-		}
-		for (j = 0; j <= len2; j++)
+		for (j = 0; j < len2; j++)
 		{
 			concat[i + j] = s2[j];
 		}
 	}
-	return (concat);
+	return (concat + '\0');
 }
