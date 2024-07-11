c205121c15884fba3366d005b5f1a1c314a0f89a
RahmanKarimli <you@example.com>
2024-07-11 15:56:12 +0400

    a

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees array
+ * @height: width of the matrix
+ * @**grid: 2D array
+ * Return: pointer to the created matrix (Success)
+ * or NULL (Error)
+ */
+void free_grid(int **grid, int height)
+{
+	int h;
+
+	for (h = 0; h < height; h++)
+	{
+		free(grid[h]);
+	}
+	free(grid);
+}
