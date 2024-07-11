13a8c09be5faf968e6aff4ec9e2aca331a5d46e4
RahmanKarimli <you@example.com>
2024-07-11 17:25:03 +0400

    free grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - frees array
  * @height: width of the matrix
- * @**grid: 2D array
+ * @grid: 2D array
  * Return: pointer to the created matrix (Success)
  * or NULL (Error)
  */
@@ -12,7 +12,7 @@ void free_grid(int **grid, int height)
 {
 	int h;
 
-	if (grid == NULL || height ==0)
+	if (grid == NULL || height == 0)
 		return;
 
 	for (h = 0; h < height; h++)
