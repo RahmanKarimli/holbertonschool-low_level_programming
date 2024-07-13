e0b970b7548cf8c2c373df2aba32044ecbdc763d
root <you@example.com>
2024-07-13 14:15:28 +0000

    a

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,16 +21,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	for (len = 0; s2[len] != '\0'; len++)
-
+	{
+	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-
+	{
+	}
 	if (n >= len)
+	{
 		n = len;
+	}
 	array  = malloc(n + len1 + 1);
 	if (array == NULL)
+	{
 		return (NULL);
+	}
 	for (j = 0; s1[j] != '\0'; j++)
+	{
 		array[j] = s1[j];
+	}
 	for (i = 0; i < n && s2[i] != '\0'; i++)
 	{
 		array[j] = s2[i];
