d027532075c7061d21031d7faa5c4e79c24cd981
root <you@example.com>
2024-07-13 14:23:22 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,15 +9,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	array = calloc(nmemb, size * nmemb);
+	array = malloc(size * nmemb);
 	if (array == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
 	}
 	return (array);
 }
