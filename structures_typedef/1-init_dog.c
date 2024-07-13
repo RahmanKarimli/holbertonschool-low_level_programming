bbede047154ec48ccfb66b94ca63ce4710c9d48e
root <you@example.com>
2024-07-13 15:53:56 +0000

    initialize dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - entry point.
+ * @d: struct
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Return: 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
