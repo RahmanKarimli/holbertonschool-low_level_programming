3688de7d30bc90f46a45875842bbf6659d5b9513
root <you@example.com>
2024-07-17 18:21:16 +0000

    simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,8 +19,8 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
+		if (strcmp(s, myop[i].op) == 0)
+			return (myop[i].f);
 		i++;
 	}
 
