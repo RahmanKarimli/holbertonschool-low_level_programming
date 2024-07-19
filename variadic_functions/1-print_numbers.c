1c467cefda7a4054f2cbca6467808a49fd6e788b
root <you@example.com>
2024-07-19 17:10:35 +0000

    sum

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,16 +14,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int num;
 
 	va_start(arr, n);
-	num = va_arg(arr, unsigned int);
-	printf("%d", num);
 	for(i = 0; i < n-1; i++)
 	{
 		num = va_arg(arr, int);
-		if (separator == NULL)
+		if (i == 0)
 		{
 			printf("%d", num);
-			continue;
 		}
-		printf("%s%d", separator, num);
+		else
+		{
+			if (separator == NULL)
+			{
+				printf("%d", num);
+			}
+			else
+			{
+				printf("%s%d", separator, num);
+			}
+		}
 	}
 }
