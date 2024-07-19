93b87cd4c24cbb00fb8225d780cabcdfce03e4b4
root <you@example.com>
2024-07-19 16:34:09 +0000

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * print_numbers - ...
+  * @n: count
+  * @seperator: seperator
+  * Return: void
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arr;
+	unsigned int i, num;
+
+	va_start(arr, n);
+	num = va_arg(arr, unsigned int);
+	printf("%d", num);
+	for(i = 0; i < n-1; i++)
+	{
+		num = va_arg(arr, int);
+		if (separator == NULL)
+		{
+			printf("%d", num);
+			continue;
+		}
+		printf("%s%d", separator, num);
+	}
+}
