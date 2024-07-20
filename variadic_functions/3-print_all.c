50c4d09bed65ee6de3747e77b7c125fe2e2972e9
root <you@example.com>
2024-07-20 17:35:29 +0000

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,3 +7,42 @@
   * Return: void
   */
 void print_all(const char * const format, ...)
+{
+	va_list arr;
+	char *str, *sep;
+	int i;
+
+	va_start(arr, format);
+	i = 0;
+	sep = "";
+	if (format != NULL)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(arr, char));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(arr, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(arr, float));
+					break;
+				case 's':
+					str = va_arg(arr, char *);
+					if (str == NULL)
+					{
+						str = "(nil)";
+					}
+					printf("%s%s", sep, str);
+					break;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(arr);
+}
