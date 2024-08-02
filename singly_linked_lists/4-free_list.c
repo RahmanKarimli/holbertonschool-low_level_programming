f5a808c15e80472c625ba2986f36182af41eb112
root <you@example.com>
2024-08-02 18:16:38 +0000

    com

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include "lists.h"
+/**
+ * free_list - ...
+ * @head: ...
+ *
+ * Return: ...
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		free(head);
+	}
+}
