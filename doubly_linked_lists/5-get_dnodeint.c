abb47d2d4a38ed2249dc77ae78dd526bc75a8b2e
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-03 22:06:22 +0400

    commit

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - ...
+ * @head: ...
+ * @index: ...
+ *
+ * Return: ...
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	int i;
+
+	tmp = head;
+	for (i = 0; i <= index; i++)
+	{
+		if (tmp)
+		{
+			tmp = tmp->next;
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (tmp->prev);
+}
