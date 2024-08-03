eefabbbecb426810781be3e763ebe954c8e222fb
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-03 12:05:57 +0400

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,8 +22,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->prev = NULL;
 	new->next = (*head);
+	if ((*head) != NULL)
+	{
+		(*head)->prev = new;
+	}
 	(*head) = new;
 	return (new);
 }
