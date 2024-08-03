ba61e406769054dbeebbdb913bba633ea75f4f4d
RahmanKarimli <rahman.karimli@icloud.com>
2024-08-03 23:50:54 +0400

    Update 5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 
 	tmp = head;
-	for (i = 0; i < index; i++)
+	for (i = 0; i <= index; i++)
 	{
 		if (tmp == NULL)
 		{
