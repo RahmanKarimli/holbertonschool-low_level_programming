799f9c42e1acf65edb91d4f210d781889caefb3d
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-08-03 15:21:22 +0400

    commit

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include "lists.h"
+/**
+ * add_dnodeint_end - ...
+ * @head: ...
+ * @n: ...
+ *
+ * Return: ...
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp, *new;
+
+	tmp = (*head);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		(*head) = new;
+		return (new);
+	}
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	return (tmp);
+}
