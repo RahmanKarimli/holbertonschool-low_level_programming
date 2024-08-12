40bfed44def7b36b60d6b75c06edb24bc44c3f53
root <you@example.com>
2024-08-12 12:06:41 +0000

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - ...
+ * @n: ...
+ *
+ * Return: ...
+ */
+void print_binary(unsigned long int n)
+{
+	int current, i, flag = 0;
+	unsigned long int result = 0;
+	for (i = 31; i >= 0; i--)
+	{
+		result = n >> i;
+		current = result & 1;
+		if (current)
+		{
+			flag = 1;
+		}
+		if (flag || (n==0 && i == 0))
+		{
+			printf("%d", current);
+		}
+	}
+}
