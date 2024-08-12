38c7a6549896c0c74e9cce53e3e68715b1b29875
root <you@example.com>
2024-08-12 10:36:24 +0000

    to unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		result = result << 1;
+		if (b[i] == '1')
+		{
+			result = result | 1;
+		}
+		else if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
