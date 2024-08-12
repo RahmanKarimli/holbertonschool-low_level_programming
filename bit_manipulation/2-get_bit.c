28eeb62c4edd5deeb024d8ad001e350f1b635fcd
root <you@example.com>
2024-08-12 12:30:05 +0000

    get bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,9 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int size;
+
+	size = sizeof(n) * 8 - 1;
 	if (n < index)
 	{
 		return (-1);
 	}
-	return ((n >> index) &1);
+	return ((n >> index) & 1);
 }
