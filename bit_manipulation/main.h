b032cca1c9132ad02683153af511b63f9896128c
root <you@example.com>
2024-08-12 10:41:59 +0000

    to unsigned int

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
