15ab21c70b048fe6a883114b1e19dd260e1894ef
root <you@example.com>
2024-08-13 15:56:55 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (len; text_content[len] != '\0'; len++)
+		for (; text_content[len] != '\0'; len++)
 		{
 		}
 	}
